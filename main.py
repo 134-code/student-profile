cdd06b751526d81b5d40b7074b303bdbeceae4da
Shifa <programer.shifa@gmail.com>
2026-09-19 18:57:56 +0300

    Create student profile

---
+++ main.py
@@ -0,0 +1,8 @@
+name= "Shifa"
+major="Computer Science"
+university= "Seiyun Universty"
+print("Student Profilr")
+print("-----------------")
+print("Name: ",name)
+print("Major: ",major)
+print("Universty: ",university)
